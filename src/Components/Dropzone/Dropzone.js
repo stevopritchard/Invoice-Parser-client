2f793a8da1c433d237a477b457a7f29fa2514c08
stevopritchard <stephen.a.pritchard@gmail.com>
2022-05-14 14:09:26 +0100

    Reformat + view of docs saved to cluster
     - Refreshed layout
     - new table gives user control over documents saved to MongoDB cluster

--- src/Components/Dropzone/Dropzone.js
+++ src/Components/Dropzone/Dropzone.js
@@ -1,38 +1,47 @@
 import React, { useCallback } from 'react';
-import { makeStyles } from '@material-ui/core/styles'
+import { Card } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { useDropzone } from 'react-dropzone';
 
 const useStyles = makeStyles(() => ({
-    dropzone: {
-        paddingLeft: '10px',
-        paddingRight: '10px'
-    }
-}))
+  dropzone: {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+}));
 
 function Dropzone({ uploadImage }) {
-    const classes = useStyles()
-    const onDrop = useCallback(acceptedFiles => { uploadImage(acceptedFiles) },[uploadImage])
+  const classes = useStyles();
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      uploadImage(acceptedFiles);
+    },
+    [uploadImage]
+  );
 
-    
-    const { 
-        getRootProps, 
-        getInputProps, 
-        isDragActive 
-    } = useDropzone({
-        multiple: true,
-        onDrop
-    })
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    multiple: true,
+    onDrop,
+  });
 
-    return (
-        <div className={classes.dropzone} {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop or click to select</p>
-            }
-        </div>
-    )
+  return (
+    <Card
+      variant="outlined"
+      style={{
+        border: isDragActive ? '2px dashed #03d1b2' : '2px dashed gray',
+      }}
+      className={classes.dropzoneCard}
+    >
+      <div className={classes.dropzone} {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop or click to select</p>
+        )}
+      </div>
+    </Card>
+  );
 }
 
 export default Dropzone;

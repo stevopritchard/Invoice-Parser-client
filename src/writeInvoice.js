974ab509014ecf3e47e1f62e5b7e62ff9407899f
stevopritchard <stephen.a.pritchard@gmail.com>
2023-02-02 09:48:20 +0000

    Added Jest.js module
    replaced the following line in package.json:
        "test": "react-scripts test",
    
    with:
        "test": "jest",

--- src/writeInvoice.js
+++ src/writeInvoice.js
@@ -2,11 +2,9 @@ import getInvoice from './getInvoice';
 import getSavedInvoices from './getSavedInvoices';
 
 const writeInvoice = async (invoice) => {
-  console.log(invoice.validRefNumber);
   const matchedNumbers = await getInvoice(invoice.validRefNumber);
-  console.log(matchedNumbers);
+
   if (!matchedNumbers && invoice.validRefNumber) {
-    console.log('truly valid');
     const response = await fetch('http://localhost:5000/writeInvoice', {
       method: 'post',
       headers: {
